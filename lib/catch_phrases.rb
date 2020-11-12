3b5ff96eb941773dc867f93d8ef4cc35ca1e5de7
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-12 16:53:07 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,13 @@
 def mario
   phrase = "It's-a me, Mario!"
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
+
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def toadstool
   puts status
 end
 
+mario 
+toadstool
