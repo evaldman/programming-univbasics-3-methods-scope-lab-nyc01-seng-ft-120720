67922c8143b078b454c519cea2fe80bf6496b722
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-12 16:59:33 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,11 +3,18 @@ def mario
   puts phrase
 end
 
-
 def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
 
-mario 
-toadstool
+def link 
+  phrase = "It's Dangerous To Go Alone! Take This"
+  puts phrase
+end
+
+def all_phrases
+  puts mario 
+  puts toadstool
+  puts link
+end
